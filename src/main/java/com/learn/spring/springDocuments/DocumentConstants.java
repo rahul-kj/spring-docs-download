eb57743702ab0233e55364ebaced4741fc4f76bf
Rahul Jain <rahulkj@gmail.com>
2013-09-19 11:44:54 -0500

    Initial commit to download all the latest PDF reference guides from spring.io

---
+++ src/main/java/com/learn/spring/springDocuments/DocumentConstants.java
@@ -0,0 +1,29 @@
+package com.learn.spring.springDocuments;
+
+public interface DocumentConstants {
+	String BASE_URL = "spring.base.url";
+	String URL = ".url";
+	String VERSION = ".version";
+	String FILE = ".file";
+
+	String SPRING_FRAMEWORK = "spring.framework";
+	String SPRING_XD = "spring.xd";
+	String SPRING_INTEGRATION = "spring.integration";
+	String SPRING_BATCH = "spring.batch";
+	String SPRING_SECURITY = "spring.security";
+	String SPRING_AMQP = "spring.amqp";
+	String SPRING_MOBILE = "spring.mobile";
+	String SPRING_ANDROID = "spring.android";
+	String SPRING_LDAP = "spring.ldap";
+	String SPRING_WEBFLOW = "spring.webflow";
+	String SPRING_SOCIAL = "spring.social";
+	String SPRING_SOCIAL_FACEBOOK = "spring.social.facebook";
+	String SPRING_SOCIAL_TWITTER = "spring.social.twitter";
+	String SPRING_SOCIAL_LINKEDIN = "spring.social.linkedin";
+	String SPRING_DATA = "spring.data";
+	String SPRING_DATA_MONGO = "spring.data.mongo";
+	String SPRING_DATA_NEO4J = "spring.data.neo4j";
+	String SPRING_DATA_GEMFIRE = "spring.data.gemfire";
+	String SPRING_DATA_REDIS = "spring.data.redis";
+	String SPRING_DATA_JDBC_EXT = "spring.data.jdbc";
+}
---
+++ src/main/java/com/learn/spring/springDocuments/SpringDocumentDownload.java
@@ -0,0 +1,155 @@
+package com.learn.spring.springDocuments;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Hello world!
+ * 
+ */
+public class SpringDocumentDownload {
+
+	private static List<String> documentList;
+	private static Properties properties;
+
+	static {
+		properties = loadAllProperties();
+		documentList = new ArrayList<String>();
+		documentList.add(DocumentConstants.SPRING_FRAMEWORK);
+		documentList.add(DocumentConstants.SPRING_XD);
+		documentList.add(DocumentConstants.SPRING_INTEGRATION);
+		documentList.add(DocumentConstants.SPRING_BATCH);
+		documentList.add(DocumentConstants.SPRING_SECURITY);
+		documentList.add(DocumentConstants.SPRING_AMQP);
+		documentList.add(DocumentConstants.SPRING_MOBILE);
+		documentList.add(DocumentConstants.SPRING_ANDROID);
+		documentList.add(DocumentConstants.SPRING_LDAP);
+		documentList.add(DocumentConstants.SPRING_WEBFLOW);
+		documentList.add(DocumentConstants.SPRING_SOCIAL);
+		documentList.add(DocumentConstants.SPRING_SOCIAL_FACEBOOK);
+		documentList.add(DocumentConstants.SPRING_SOCIAL_TWITTER);
+		documentList.add(DocumentConstants.SPRING_SOCIAL_LINKEDIN);
+		documentList.add(DocumentConstants.SPRING_DATA);
+		documentList.add(DocumentConstants.SPRING_DATA_MONGO);
+		documentList.add(DocumentConstants.SPRING_DATA_NEO4J);
+		documentList.add(DocumentConstants.SPRING_DATA_GEMFIRE);
+		documentList.add(DocumentConstants.SPRING_DATA_REDIS);
+		documentList.add(DocumentConstants.SPRING_DATA_JDBC_EXT);
+	}
+
+	public static void main(String[] args) {
+		if(args == null || args.length != 1) {
+			System.out.println("Cannot proceed, please specify the output directory");
+			System.exit(0);
+		}
+
+		HttpClient httpClient = new DefaultHttpClient();
+
+		for (String document : documentList) {
+			String baseURL = properties.getProperty(DocumentConstants.BASE_URL);
+			String url = properties.getProperty(document + DocumentConstants.URL);
+			String version = properties.getProperty(document + DocumentConstants.VERSION);
+
+			url = MessageFormat.format(url, baseURL, version);
+
+			String fileName = properties.getProperty(document + DocumentConstants.FILE);
+			String directory = args[0];
+			File file = new File(directory + fileName);
+			if (!file.exists()) {
+				HttpGet httpGet = new HttpGet(url + fileName);
+
+				try {
+					createDirectoryIfItDoesNotExist(directory);
+					fetchAndStoreFile(httpClient, file, httpGet);
+				} catch (Exception e) {
+					System.out.println("Failed to download file : " + fileName + "due to : " + e);
+					continue;
+				}
+
+				System.out.println("Done with file download for : " + fileName);
+			} else {
+				System.out.println("File already exists : " + fileName);
+			}
+			System.out.println("************** SEPERATOR *****************");
+		}
+
+	}
+
+	private static void fetchAndStoreFile(HttpClient httpClient, File file, HttpGet httpGet) throws IOException,
+			ClientProtocolException, FileNotFoundException {
+		HttpResponse response = httpClient.execute(httpGet);
+		InputStream pdfStream = response.getEntity().getContent();
+
+		FileOutputStream fos = new FileOutputStream(file);
+
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		while ((read = pdfStream.read(bytes)) != -1) {
+			fos.write(bytes, 0, read);
+		}
+
+		fos.close();
+
+		pdfStream.close();
+	}
+
+	private static void createDirectoryIfItDoesNotExist(String directory) {
+		File dir = new File(directory);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+	}
+
+	private static Properties loadAllProperties() {
+		Properties properties = new Properties();
+		loadAllUrls(properties);
+		loadAllVersions(properties);
+		loadAllFilesToDownload(properties);
+		return properties;
+	}
+
+	private static void loadAllUrls(Properties properties) {
+		ResourceBundle bundle = ResourceBundle.getBundle("META-INF/spring/url");
+
+		Enumeration<String> keys = bundle.getKeys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			properties.put(key, bundle.getString(key));
+		}
+	}
+
+	private static void loadAllFilesToDownload(Properties properties) {
+		ResourceBundle bundle = ResourceBundle.getBundle("META-INF/spring/file");
+
+		Enumeration<String> keys = bundle.getKeys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			properties.put(key, bundle.getString(key));
+		}
+	}
+
+	private static void loadAllVersions(Properties properties) {
+		ResourceBundle bundle = ResourceBundle.getBundle("META-INF/spring/version");
+
+		Enumeration<String> keys = bundle.getKeys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			properties.put(key, bundle.getString(key));
+		}
+	}
+}
